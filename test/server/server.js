98ad681cac515f111150f1f89b2c74bcf4650ec3
Ani Channarasappa <ani@locust.dev>
2019-12-22 13:32:12 -0500

    Fixed styling issue for test and added pre-commit hook

--- test/server/server.js
+++ test/server/server.js
@@ -1,5 +1,6 @@
 const Hapi = require('@hapi/hapi');
 const Vision = require('@hapi/vision');
+const handlebars = require('handlebars');
 const { readFileSync } = require('fs');
 
 const MAX_EXTRA_LATENCY = 1000;
@@ -10,48 +11,58 @@ const addRoutes = async (server) => {
   await server.register(Vision);
 
   server.views({
-    engines: { html: require('handlebars') },
-    path: __dirname + '/templates',
+    engines: { html: handlebars },
+    path: `${__dirname}/templates`,
   });
 
-  JSON.parse(readFileSync(__dirname + '/pages.json'))
-  .map(({ title, links, depth, path }) => {
-
-    server.route({
-      method: 'GET',
+  JSON.parse(readFileSync(`${__dirname}/pages.json`))
+    .forEach(({
+      title,
+      links,
+      depth,
       path,
-      handler: (r, h) => new Promise((resolve, reject) => {
-        
-        setTimeout(() => resolve(h.view('page', {
-          title,
-          links,
-          depth,
-        })), Math.floor(Math.random() * (MAX_EXTRA_LATENCY - MIN_EXTRA_LATENCY + 1) + MIN_EXTRA_LATENCY));
+    }) => {
 
-      })
+      server.route({
+        method: 'GET',
+        path,
+        handler: (r, h) => new Promise((resolve) => {
+
+          setTimeout(() => resolve(h.view('page', {
+            title,
+            links,
+            depth,
+          })),
+          Math.floor(
+            Math.random() * (MAX_EXTRA_LATENCY - MIN_EXTRA_LATENCY + 1) + MIN_EXTRA_LATENCY,
+          ));
+
+        }),
+      });
+
     });
-
-  });
 
 };
 
 const init = async () => {
 
   const server = Hapi.server({
-      port: 3001,
-      host: 'localhost'
+    port: 3001,
+    host: 'localhost',
   });
 
   await addRoutes(server);
 
   await server.start();
   console.log('Test server running on %s', server.info.uri);
+
 };
 
 process.on('unhandledRejection', (err) => {
 
   console.log(err);
   process.exit(1);
+
 });
 
 init();
